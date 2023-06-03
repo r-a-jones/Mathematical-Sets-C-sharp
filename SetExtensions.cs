90043685c5d567cbfa5e7e3165865b9b9bf92da6
Robin Jones <67515128+r-a-jones@users.noreply.github.com>
2023-06-03 13:31:47 +0100

    Set extensions

---
+++ SetExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mathematical_Sets
+{
+	public static class SetExtensions
+	{
+		/// <summary>
+		/// Returns the given array as a set.
+		/// </summary>
+		public static Set ToSet(this object[] array)
+		{
+			Set set = new Set();
+			set.Add(array);
+			return set;
+		}
+
+		/// <summary>
+		/// Returns the given array as a set.
+		/// </summary>
+		public static Set ToSet<T>(this T[] array)
+		{
+			return ToSet(array);
+		}
+	}
+}
